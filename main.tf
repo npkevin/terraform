ff336c0822c7d74e50a7506e7fb93744fc225cbd
Kevin Nguyen <kevnguyenp@gmail.com>
2026-01-06 18:05:32 +0000

    seperate into files

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "Telmate/proxmox"
-      version = "3.0.2-rc05"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url          = "https://192.168.2.100:8006/api2/json"
-  pm_api_token_id     = "terraform@pve!terraform"
-  pm_api_token_secret = "dfa4c007-7332-45c5-995a-bb03a78e85f3"
-  pm_tls_insecure     = true
-}
-
 module "lvm" {
   source    = "./modules/proxmox-lvm"
   for_each  = var.proxmox_lvm
